19cef86c3276c67a8f5a30431333b7f94c4fa686
Pakin49 <philgus884@gmail.com>
2025-02-14 15:49:58 +0700

    assignment3

--- button_interrupt.py
+++ button_interrupt.py
@@ -6,18 +6,9 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 button_pin = 5
 GPIO.setup(button_pin,GPIO.IN,pull_up_down = GPIO.PUD_UP)
-led_pin = 7
-GPIO.setup(led_pin,GPIO.OUT)
-led = 0
 
 def button_pressed_callback(chennel):
-	global led
-	if(led==0):
-		GPIO.output(led_pin,GPIO.HIGH)
-		led = 1
-	else:
-		GPIO.output(led_pin,GPIO.LOW)
-		led = 0
+	print("button pressed")
 
 GPIO.add_event_detect(button_pin,GPIO.FALLING,button_pressed_callback, bouncetime=200)
 
